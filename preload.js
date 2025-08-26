07b9d5043a98e5050188e868e200f8922b37fe0b
Axector <dansvjat021100@gmail.com>
2025-08-26 22:43:06 +0300

    test with locations

--- preload.js
+++ preload.js
@@ -1,7 +1,7 @@
-import EventController from '/src/utils/EventController';
-import ComponentRenderer from '/src/utils/ComponentRenderer';
-import History from '/src/utils/History';
-import DataBaseController from '/src/utils/DataBaseController';
+import EventController from './src/utils/EventController';
+import ComponentRenderer from './src/utils/ComponentRenderer';
+import History from './src/utils/History';
+import DataBaseController from './src/utils/DataBaseController';
 
 window.LESCH = {};
 window.LESCH.EventController = new EventController();
